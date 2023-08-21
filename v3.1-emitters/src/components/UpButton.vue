b569a3a9d5923795a8a7698f42b58c59d23063d1
Clemens Bruesch <clemensbruesch@gmail.com>
2023-08-21 20:22:10 +0200

    most basic emitter

---
+++ v3.1-emitters/src/components/UpButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ComponentContainer">
+<button @click="clickHandler">UP</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    emits: ['somebodyJustClickedTheButton'],
+    methods: {
+      clickHandler() {
+        this.$emit('somebodyJustClickedTheButton')
+      }
+    }
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
