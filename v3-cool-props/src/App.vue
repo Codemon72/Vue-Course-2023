cab4887e26b527ef5e85d45e4504d2cd0c9a22c9
Clemens Bruesch <clemensbruesch@gmail.com>
2023-08-21 18:43:17 +0200

    input with direct value relay

---
+++ v3-cool-props/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" @input="inputHandler" />
+  </div>
+  <div>{{ inputValue }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    }
+  },
+  methods: {
+    inputHandler(event) {
+      this.inputValue = event.target.value
+    }
+  }
+}
+</script>
+
+<style scoped></style>
