5de3aeb2d97de58ea4d6632238fc99d5fb287485
Clemens Bruesch <clemensbruesch@gmail.com>
2023-08-25 20:59:07 +0200

    basic app with API call

---
+++ v4.2_exercise_API_call/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else>
+    <div class="flex">
+      <MovieCard v-for="movie in movies" :movie="movie" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieCard from './components/MovieCard.vue'
+
+export default {
+  components: {
+    MovieCard
+  },
+  data() {
+    return {
+      isLoading: true,
+      movies: []
+    }
+  },
+  async created() {
+    const response = await fetch('https://swapi.dev/api/films')
+    const result = await response.json()
+
+    this.movies = result.results
+    this.isLoading = false
+
+    console.log(this.movies)
+  }
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+</style>
---
+++ v4.2_exercise_API_call/src/components/MovieCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <h3>{{ movie.title }}</h3>
+    <bold>Episode: {{ movie.episode_id }}</bold> <br><br>
+    <div>{{ movie.opening_crawl }}</div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      movie: Object
+    }
+    
+  }
+</script>
+
+<style scoped>
+.card {
+  min-height: 100px;
+  width: 250px;
+  border-radius: 6px;
+  border: 1px solid black;
+  box-shadow: 5px 5px 10px gray;
+  padding: 1rem;
+}
+
+</style>
