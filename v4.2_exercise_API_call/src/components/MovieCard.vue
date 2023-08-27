1c974a645b6995431e7bbbb3bb4756b7680eaf4d
Clemens Bruesch <clemensbruesch@gmail.com>
2023-08-27 13:43:41 +0200

    comments added

--- v4.2_exercise_API_call/src/components/MovieCard.vue
+++ v4.2_exercise_API_call/src/components/MovieCard.vue
@@ -2,12 +2,17 @@
   <div class="card">
     <h3>{{ movie.title }}</h3>
     <b>Episode: {{ movie.episode_id }}</b> <br /><br />
+    <!-- If showMore is true, display the entire opening crawl -->
     <div v-if="showMore">
       <div v-html="movie.opening_crawl"></div>
+      <!-- Clicking this will toggle the showMore value to false -->
       <span @click="toggleShowMore" class="show-more">Show less</span>
     </div>
+    <!-- If showMore is false, display only the shortText -->
     <div v-else>
+      <!-- Use v-html to render HTML content -->
       <div class="fade-out" v-html="shortText"></div>
+      <!-- Clicking this will toggle the showMore value to true -->
       <span @click="toggleShowMore" class="show-more">Show more</span>
     </div>
   </div>
@@ -20,17 +25,21 @@ export default {
   },
   data() {
     return {
+      // This data property controls whether the entire opening crawl or only the shortText is displayed
       showMore: false
     }
   },
   computed: {
     shortText() {
+      // Split the opening_crawl into an array of words
       const words = this.movie.opening_crawl.split(' ')
+      // Return the first 50 words as a string, followed by '...'
       return words.slice(0, 50).join(' ') + '...'
     }
   },
   methods: {
     toggleShowMore() {
+      // Toggle the value of showMore
       this.showMore = !this.showMore
     }
   }
