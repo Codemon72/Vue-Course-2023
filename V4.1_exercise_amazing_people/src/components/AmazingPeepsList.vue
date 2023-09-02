bb2bb3ec672fbbfdf826cf574cf7e1a453364676
Clemens Bruesch <clemensbruesch@gmail.com>
2023-09-02 19:11:44 +0200

    feat: added content

--- V4.1_exercise_amazing_people/src/components/AmazingPeepsList.vue
+++ V4.1_exercise_amazing_people/src/components/AmazingPeepsList.vue
@@ -27,6 +27,12 @@ const amazingPeeps = [
     role: 'Persistent Problem Solver',
     img_src: 'https://www.agriculture.com/thmb/eVOCUlnXnqIy2WJ0QbRu2k_LLGw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/allan-savory-9b13936838374bf4b6e23abae5eaca8c.jpg',
     description: 'Clifford Allan Redin Savory is a Zimbabwean livestock farmer and president of the Savory Institute. He originated holistic management and gave a very inspiring Ted Talk about it.'
+  },
+  {
+    name: 'Sam Childers',
+    role: 'Badass Preacher',
+    img_src: 'https://static.wixstatic.com/media/949f3d_9e33a641566a4752bc8ebc736bdd7f59~mv2.jpg/v1/fit/w_440,h_600,q_90/949f3d_9e33a641566a4752bc8ebc736bdd7f59~mv2.jpg',
+    description: 'Founder of "Angels of East Africa" and built an orphanage in Sudan to rescue and care for children who were affected by violence, poverty, and conflict. His work has involved rescuing child soldiers, rehabilitating them, and providing education and vocational training.'
   }
 ]
 
